74dca6ff3460095603a3e5284f49a38a48e77da2
= <=>
2026-04-05 17:38:22 -0400

    better status script

--- status.sh
+++ status.sh
@@ -11,3 +11,10 @@ aws resourcegroupstaggingapi get-resources --tag-filters \
     --output table \
 
 
+echo "[ ===== Bucket Status ===== ]"
+aws s3api head-bucket --bucket biteroll-static-site-sawyer
+
+echo "[ === CloudFront Status === ]"
+aws cloudfront list-distributions \
+    --query 'DistributionList.Items[*].{Status:Status,Enabled:Enabled,Domain:DomainName}' \
+    --output table
