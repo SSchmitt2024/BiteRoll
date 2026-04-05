f012fc314998ae9a2785bd70f8af7b9be025a440
= <=>
2026-04-05 19:44:32 -0400

    dep

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
+success = 1
+
 aws cloudformation deploy \
     --template-file ../Infrastructure/S3-Static-Site.yaml \
     --stack-name biteroll-s3-static-site
+success=$?
 aws s3 cp ../frontend/index.html s3://biteroll-static-site-sawyer/ #attaches index.html to bucket
+success=$?
 
 aws cloudformation deploy \
     --template-file ../Infrastructure/CloudFront.yaml \
     --stack-name biteroll-cloudfront
+success=$?
 
 aws cloudformation deploy \
     --template-file ../Infrastructure/BucketPolicy.yaml \
     --stack-name biteroll-s3-policy
+success=$?
 
 aws cloudformation deploy \
     --template-file ../Infrastructure/Cognito.yaml \
     --stack-name biteroll-cognito
+success=$?
 
-echo ""
+if [ success -eq 1]; then
+    echo -e "\nResources Deployed Successfully.\n"
+else
+    echo -e "\nResources Failed to Deployed.\n"
+
 ./status.sh
 
