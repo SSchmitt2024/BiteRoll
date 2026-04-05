9375c0dc55f2536b36335d539b8f997fa8317f86
= <=>
2026-04-05 17:59:00 -0400

    loop at the end to give final status

--- status.sh
+++ status.sh
@@ -10,11 +10,25 @@ aws resourcegroupstaggingapi get-resources --tag-filters \
     --query 'ResourceTagMappingList[*].{ARN:ResourceARN}' \
     --output table \
 
+declar -A resources
 
 echo -e "\n[ ===== Bucket Status ===== ]"
 aws s3api head-bucket --bucket biteroll-static-site-sawyer
+S3_Status = $?
+resources["S3"] = $S3_Status
 
 echo -e "\n[ === CloudFront Status === ]"
 aws cloudfront list-distributions \
     --query 'DistributionList.Items[*].{Status:Status,Enabled:Enabled,Domain:DomainName}' \
     --output table
+CF_Status = $?
+resources["CloudFront"] = $CF_Status
+
+for resource in "${!resources[@]}"; do
+    if [ ${resources[$resource]} -eq 0 ]; then
+        echo "$resource : Running"
+    else
+        echo "$resource : ERROR"
+    fi
+done
+
