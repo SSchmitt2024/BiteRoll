dcc05aa69cac98763dbeff7ecf6e6738884c64f7
= <=>
2026-04-05 18:00:17 -0400

    fix

--- status.sh
+++ status.sh
@@ -10,19 +10,19 @@ aws resourcegroupstaggingapi get-resources --tag-filters \
     --query 'ResourceTagMappingList[*].{ARN:ResourceARN}' \
     --output table \
 
-declar -A resources
+declare -A resources
 
 echo -e "\n[ ===== Bucket Status ===== ]"
 aws s3api head-bucket --bucket biteroll-static-site-sawyer
-S3_Status = $?
-resources["S3"] = $S3_Status
+S3_Status=$?
+resources["S3"]=$S3_Status
 
 echo -e "\n[ === CloudFront Status === ]"
 aws cloudfront list-distributions \
     --query 'DistributionList.Items[*].{Status:Status,Enabled:Enabled,Domain:DomainName}' \
     --output table
-CF_Status = $?
-resources["CloudFront"] = $CF_Status
+CF_Status=$?
+resources["CloudFront"]=$CF_Status
 
 for resource in "${!resources[@]}"; do
     if [ ${resources[$resource]} -eq 0 ]; then
