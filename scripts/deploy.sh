ca55f55a59996537e1ac4dd0bb27ee32536a93bb
= <=>
2026-04-05 19:40:15 -0400

    fixed paths

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 aws cloudformation deploy \
-    --template-file S3-Static-Site.yaml \
+    --template-file ../Infrastructure/S3-Static-Site.yaml \
     --stack-name biteroll-s3-static-site
 aws s3 cp index.html s3://biteroll-static-site-sawyer/ #attaches index.html to bucket
 
 aws cloudformation deploy \
-    --template-file CloudFront.yaml \
+    --template-file ../Infrastructure/CloudFront.yaml \
     --stack-name biteroll-cloudfront
 
 aws cloudformation deploy \
-    --template-file BucketPolicy.yaml \
+    --template-file ../Infrastructure/BucketPolicy.yaml \
     --stack-name biteroll-s3-policy
 
 aws cloudformation deploy \
-    --template-file Cognito.yaml \
+    --template-file ../Infrastructure/Cognito.yaml \
     --stack-name biteroll-cognito
 
 echo ""
