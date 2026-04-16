fde7d7da17920c0aa9d7350781b70f4e057fca2f
sschmitt2024 <sawyerals.nh@gmail.com>
2026-04-15 20:06:44 -0400

    fix deploy script to actually build react frontend and sync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
+set -e
+
+aws cloudformation deploy \
+    --template-file ../Infrastructure/Cognito.yaml \
+    --stack-name biteroll-cognito
+
+echo "Fetching Cognito config from CloudFormation..."
+export VITE_USER_POOL_ID=$(aws cloudformation list-exports \
+    --query "Exports[?Name=='CognitoUserPoolID'].Value" --output text)
+export VITE_CLIENT_ID=$(aws cloudformation list-exports \
+    --query "Exports[?Name=='CognitoUserPoolClient'].Value" --output text)
+echo "  UserPoolId: $VITE_USER_POOL_ID"
+echo "  ClientId:   $VITE_CLIENT_ID"
+
+cd ../frontend
+npm ci
+npm run build
+
 aws cloudformation deploy \
     --template-file ../Infrastructure/S3-Static-Site.yaml \
     --stack-name biteroll-s3-static-site
-aws s3 cp ../frontend/index.html s3://biteroll-static-site-sawyer/ #attaches index.html to bucket
-
-aws cloudformation deploy \
-    --template-file ../Infrastructure/CloudFront.yaml \
-    --stack-name biteroll-cloudfront
+aws s3 sync dist s3://biteroll-static-site-sawyer/ --delete
 
 aws cloudformation deploy \
     --template-file ../Infrastructure/BucketPolicy.yaml \
     --stack-name biteroll-s3-policy
 
 aws cloudformation deploy \
-    --template-file ../Infrastructure/Cognito.yaml \
-    --stack-name biteroll-cognito
+    --template-file ../Infrastructure/CloudFront.yaml \
+    --stack-name biteroll-cloudfront
+
 
 echo ""
+cd ../scripts
 ./status.sh
 
