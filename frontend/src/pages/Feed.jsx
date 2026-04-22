0ac29fd82bb1af0e1bedc76a9354bc0ad0cad46d
sschmitt2024 <sawyerals.nh@gmail.com>
2026-04-22 17:26:13 -0400

    fix

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -24,6 +24,10 @@ export default function Feed() {
                     video
                 }))
             )
+            for (let i = cards.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [cards[i], cards[j]] = [cards[j], cards[i]]
+            }
             setVideoCards(cards)
         })
     }
